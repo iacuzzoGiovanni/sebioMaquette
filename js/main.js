17777b1d92a2df64295c51d231af856a317c53cb
Iacuzzo Giovanni <iacuzzogiovanni@gmail.com>
2013-01-20 21:22:19 +0100

    accueil presque fini, manque megadropdown, panier, connexion

---
+++ js/main.js
@@ -0,0 +1,31 @@
+/* JS Document
+ * coded by GIOVANNI IACUZZO 2383
+ * 2013
+ */
+
+/*jshint nonstandard: true, browser: true, boss: true */
+/*global jQuery */
+
+( function ( $ ) {
+	"use strict";
+
+	// --- global vars
+	
+	
+	// --- methods
+	var slide = function(e){
+		e.preventDefault();
+		$("#changeSlide a").css('backgroundImage', 'none');
+		$(this).css('backgroundImage', 'url("./img/arrowSlide.png")');
+		$("#highlited article").hide();
+		$('#highlited article[data-index="' + $(this).attr("data-index") + '"]').show();
+	};
+
+	$( function () {
+		// --- onload routines
+		$("#changeSlide li:first-child a").css('backgroundImage', 'url("./img/arrowSlide.png")');
+		$("#changeSlide a").on("click", slide);
+	} );
+}( jQuery ) );
+
+
